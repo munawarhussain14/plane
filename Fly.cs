cbef85cd970a43036f11e0a57a9352e0241d7884
munawarhussain14 <munawarhussain10@gmail.com>
2018-10-25 20:29:32 +0500

    plane code

---
+++ Fly.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour {
+
+    public GameObject target;
+    public float rotationSpeed;
+    public float speed;
+    Rigidbody2D rg2d;
+
+    // Use this for initialization
+    void Start () {
+        rg2d = GetComponent<Rigidbody2D>();
+        //InvokeRepeating("changePosition",5,5);
+    }
+
+    void changePosition()
+    {
+        float x = Random.Range(-9.0f,9.0f);
+        float y = Random.Range(-6.0f, 6.0f);
+        target.transform.position = new Vector3(x,y,0);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 vectorToTarget = target.transform.position - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        Quaternion qt = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, qt, Time.deltaTime * rotationSpeed);
+        rg2d.velocity = transform.right*speed;
+        float dist = Vector3.Distance(target.transform.position, transform.position);
+        if (dist < 2)
+        {
+            changePosition();
+        }
+    }
+}
